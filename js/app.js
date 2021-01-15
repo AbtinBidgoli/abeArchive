bf7864aff54e775f3584f2b7e8f1f22c72ac3c08
AbtinBidgoli <37858157+AbtinBidgoli@users.noreply.github.com>
2021-01-15 00:30:37 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,6 +11,17 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
+jQuery('.faq-q').click(function(){
+				if (jQuery(this).siblings().find('.faq-a').is(':visible')) {
+					jQuery(this).removeClass('faq-q-open');
+					jQuery(this).siblings().find('.faq-a').removeClass('faq-a-open').slideUp();
+				} 
+				else {
+					jQuery(this).addClass('faq-q-open');
+					jQuery(this).siblings().find('.faq-a').addClass('faq-a-open').slideDown();
+				}
+				})
+
 particlesJS('particles-js',
   
   {
